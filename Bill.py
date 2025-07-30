c1f80672bbcf6c6df729b86a6a81989ec78d3579
austinrtn <kolowemployment@gmail.com>
2025-07-30 18:18:19 -0400

    change

--- Bill.py
+++ Bill.py
@@ -1,38 +1,97 @@
 import uuid
+import time
+import webbrowser
+import os 
+import json 
 
 filename = "bill_data.json"
-bills = None
-unpaid_bills = None
 
 class Bill:
+    @staticmethod
+    def json_to_bill(data):
+        bill = Bill(data["name"], data["amount"], data["due_date"], data['website'], paid=data["paid"], uid=data["uid"])
+        return bill
+
+    @staticmethod
     def get_all_bills():
         bills = []
         if os.path.exists(filename):
             with open(filename, "r") as f: 
                 try: 
-                    bills = json.load(f)
+                    bill_data = json.load(f)
+                    for data in bill_data:
+                        bills.append(Bill.json_to_bill(data))
+
                 except json.JSONDecodeError:
                     bills = []
 
                 return bills
                 
-    def get_unpaid_bills
-        if unpaid_bills is not None: return unpaid_bills   
+    @staticmethod
+    def get_unpaid_bills():
+        unpaid_bills = []
+        all_bills = Bill.get_all_bills()
+        for bill in all_bills:
+            if bill.paid is not True: 
+                unpaid_bills.append(bill)
 
+        return unpaid_bills
+
+    @staticmethod
     def add_bill(bill):
+        bills = Bill.get_all_bills()
+        bills.append(bill)
 
+        Bill.save(bills)
+
+
+    @staticmethod
     def delete_bill(bill):
-        #delete
+        bills = Bill.get_all_bills()
 
-    def __init__(self, name, amount, due_date, website, uid=None):
+        for i, other in enumerate(bills):
+            if bill.uid == other.uid:
+                del bills[i]
+
+        Bill.save(bills)
+
+    @staticmethod
+    def edit_bill(bill):
+        bills = Bill.get_all_bills()
+        for other in bills:
+            if bill.uid != other.uid: continue
+            for attr, value in vars(bill).items():
+                setattr(other, attr, value)
+            break
+
+        Bill.save(bills)
+
+    @staticmethod
+    def mark_all_as_unpaid():
+        bills = Bill.get_all_bills()
+        for bill in bills:
+            bill.paid = False
+
+        Bill.save(bills)
+
+    @staticmethod
+    def save(bills):
+        bill_data = [bill.get_json() for bill in bills]
+        with open(filename, "w") as f:
+            json.dump(bill_data, f, indent=2)
+
+    def __init__(self, name, amount, due_date, website, paid=False, uid=None):
         self.name = name 
         self.amount = amount
         self.due_date = due_date
         self.website = website 
 
-        self.paid= False
+        self.paid = paid
         if uid is None: self.uid = str(uuid.uuid4()) 
         else: self.uid = uid 
+
+    def open_website(self):
+        webbrowser.open(self.website)
 
     def get_json(self):
         return {
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os 
+import math
 import time
 import json
 import readchar
@@ -19,9 +20,9 @@ def clr():
 def main_menu():
     while True:
         clr()
-        console.print("[bold cyan][1] view bills")
-        console.print("[bold green][2] add bill")
-        console.print("[bold red][[q]] exit")
+        console.print("[bold cyan][1]VIEW BILLS")
+        console.print("[bold green][2]ADD BILL")
+        console.print("[bold red][[q]]EXIT")
 
         key = readchar.readkey()
         if key == "1":
@@ -45,34 +46,58 @@ def view_bills():
 
     while True:
         clr()
-        global page_index
-        bills = []
-        with open(filename, "r") as f: 
-            try: 
-                json_bills = json.load(f)
-                for bill in json_bills:
-                    bills.append(Bill(bill["name"], bill["amount"], bill["due_date"], bill['website'], uid=bill["uid"]))
-            except json.JSONDecodeError:
-                bills = []
-        
+        global page_index, show_all_bills
+        if not show_all_bills: bills = Bill.get_unpaid_bills()
+        else: bills = Bill.get_all_bills()
+
         bills_in_page = pagify(bills, page_index, max_items_in_page)
 
-        table = get_bill_table(bills_in_page)
+        table = get_bill_table(bills_in_page, len(bills))
         console.print(table)
+        print_table_bindings()
 
         key = readchar.readkey()
-        if key == readchar.key.RIGHT: page_index += 1
-        elif key == readchar.key.LEFT: page_index -=1 
+        
+        amount_of_pages = math.ceil(len(bills) / max_items_in_page)
+
+        if key == readchar.key.RIGHT: 
+            page_index += 1
+            if page_index > amount_of_pages:
+                page_index = 0
+
+        elif key == readchar.key.LEFT: 
+            page_index -=1 
+            if page_index < 0:
+                page_index = amount_of_pages-1
+
+
+        elif key == "f":
+            if show_all_bills: show_all_bills = False
+            else: show_all_bills = True
+
+        elif key == "r":
+            clr()
+            console.print("[bold][yellow]Are you sure you want to mark all bills as unpaid?[/yellow] [cyan]Y[/cyan]/[red]N[/red][/bold]")
+            confirm = readchar.readkey()
+            if confirm == "y": 
+                Bill.mark_all_as_unpaid() 
+            elif confirm == "n":
+                continue
+
         elif key.isdigit():
             i = int(key)
             selected_bill = bills_in_page[i]
             select_bill(selected_bill)
+
         elif key == "q": break
     
-def get_bill_table(bills):
-    table = Table(title="Bills")
+def get_bill_table(bills, total_bill_count):
+    total_pages = math.ceil(total_bill_count / max_items_in_page)
+    current_page = page_index + 1  # assuming page_index starts at 0
+    title = f"BILLS [{current_page}/{total_pages}]"
     col_color = "cyan"
 
+    table = Table(title=title)
     table.add_column("#")
     table.add_column("Name", style=col_color)
     table.add_column("Amount", style=col_color)
@@ -90,14 +115,94 @@ def get_bill_table(bills):
     
     return table
 
+def print_table_bindings():
+    console.print(f"\n\nLast Page / Next Page [<-/->]")
+    if show_all_bills:
+        console.print("Show Unpaid Bills [F]")
+    else:
+        console.print("Show All Bills [F]")
+    console.print("Mark All As Unpaid[R]") 
+
 def select_bill(bill):
     while True:
         clr()
+
+        bill_status = "Unpaid" if bill.paid  else "Paid"
+        
         console.print(f"[bold]{bill.name} | ${bill.amount}[/bold]")
-        console.print(f"[yellow][1]Mark Paid[/yellow]")
-        console.print(f"[green][2]Open Website[/yellow]")
-        console.print(f"[red][3]Delete[/yellow]")
-        readchar.readkey()
+        console.print(f"[green][1]Mark {bill_status}[/green]")
+        console.print(f"[yellow][2]Edit Bill[/yellow]")
+        console.print(f"[cyan][3]Open Website[/cyan]")
+        console.print(f"[red][4]Delete[/red]")
+        console.print(f"[q]Back")
+        key = readchar.readkey()
+
+        if key == "1":
+            if bill.paid:
+                bill.paid = False
+            else: bill.paid = True
+            Bill.edit_bill(bill)
+            console.print("[green]Bill marked as paid![/green]")
+        elif key == "2":
+            edit_bill(bill)
+        elif key == "3":
+            bill.open_website()
+        elif key == "4": 
+            while True:
+                clr()
+                console.print("[bold]Do you really want to delete bill? [cyan]Y[/cyan]/[red]N[/red][/bold]")
+                confirm = readchar.readkey()
+
+                if confirm == "y":
+                    Bill.delete_bill(bill)
+                    return
+                elif confirm == "n":
+                    break
+
+        elif key == "q":
+            break
+
+def edit_bill(bill):
+    while True:
+        clr()
+        console.print("[bold]Select to edit[/bold]")
+        console.print("[1]Name")
+        console.print("[2]Amount")
+        console.print("[3]Due Date")
+        console.print("[4]Website")
+        console.print("[q]Back")
+        key = readchar.readkey()
+
+        if key == "q":
+            return
+
+        clr()
+        console.print("[bold]Enter New Value:[/bold]")
+        value = input()
+
+        if key == "1":
+            bill.name = value
+
+        elif key == "2":
+            try: 
+                value = float(value)
+                bill.amount =  value   
+            except VallueError:
+                console.print("[bold red]Error: Non-numeric value entered.[/bold red]")
+                readchar.readkey()
+
+        elif key == "3":
+            value = format_date(value)
+            if not value: 
+                readchar.readkey()
+                continue
+            bill.due_date = value
+                
+        elif key == "4": 
+            bill.website = value
+
+
+        Bill.edit_bill(bill)
 
 def add_bill():
     while True:
@@ -116,7 +221,7 @@ def add_bill():
         try:
             amount = float(amount)
         except ValueError:
-            console.print("[bold red]Input cannot contain non-numeric characters.[/bold red]")
+            console.print("[bold red]Error: Non-numeric value entered.[/bold red]")
             readchar.readkey()
             continue
 
@@ -124,20 +229,11 @@ def add_bill():
 
         console.print("Day of the month bill is due: ")
         due_date = input()
+        due_date = format_date(due_date)
 
-        try:
-            int_date = int(due_date)
-            if not 1 <= int_date <= 31:
-                raise ValueError
-            if int_date < 10:
-                due_date = "0" + due_date
-
-
-        except ValueError:
-            console.print("[bold red]Incorrect Date[/bold red]") 
+        if due_date is None:
             readchar.readkey()
             continue
-
         clr()
 
         console.print("Website: ")
@@ -155,23 +251,23 @@ def add_bill():
             k = key.lower()
             if k == "y":
                 bill = Bill(name, amount, due_date, website)
-                append_bill_to_file(bill)
+                Bill.add_bill(bill)
                 return True
             elif k == "n":
                 break
 
-def append_bill_to_file(bill):
-    data = bill.get_json()
-    bills = []
+def format_date(date):
+    try:
+        int_date = int(date)
+        if not 1 <= int_date <= 31:
+            raise ValueError
+        if int_date < 10:
+            date = "0" + date
+        return date
 
-
-    bills.append(data)
-
-    with open(filename, "w") as f:
-        json.dump(bills, f, indent=2)
-
-    console.print("[bold green] Bill saved successfully!  Press any key to continue...")
-    readchar.readkey()
+    except ValueError:
+        console.print("[bold red]Error:Inccorect data entered.[/bold red]") 
+        return None 
 
 if __name__ == "__main__":
     main_menu()
