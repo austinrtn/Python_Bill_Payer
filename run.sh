dea6fd36b0fbe34ceba4417714bed05da815f9c1
austinrtn <kolowemployment@gmail.com>
2025-09-01 14:01:57 -0400

    Added run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
 venv_directory="./.venv"
 
-if ! command -v python3 > /dev/null 2>&1; then
-	echo "Python3 not installed... installing"
-	sudo apt install python3 
-fi
-
-if ! command -v pip3 > /dev/null 2>&1; then
-	echo "Pip3 not installed... installing"
-	sudo apt install python3-pip3
-fi
-
 if [ ! -d "$venv_directory" ]; then
 	echo "Initializing Python3 Virtual Enviornment..."
 	python3 -m venv .venv
