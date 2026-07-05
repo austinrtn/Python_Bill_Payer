068cca9cd2e315e2c253f4638c130c83d6933102
austinrtn <kolowemployment@gmail.com>
2026-07-05 18:04:29 -0400

    Fixed relative path issue

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")" && pwd)"
+
+cd "$SCRIPT_DIR"
+
 venv_directory="./.venv"
 
 if [ ! -d "$venv_directory" ]; then
